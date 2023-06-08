e09761ffcdf78dd226f693311d60890d030b031c
OpaChaUA <opaCha.ua.94@gmail.com>
2023-06-08 22:17:24 +0300

    new cod

--- src/main/java/modul9/TimeServlet.java
+++ src/main/java/modul9/TimeServlet.java
@@ -2,20 +2,23 @@ package modul9;
 
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
-
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
-import org.thymeleaf.templateresolver.FileTemplateResolver;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
-@WebServlet("/test")
+@WebServlet("/time")
 public class TimeServlet extends HttpServlet {
     private TemplateEngine engine;
+
     @Override
     public void init() throws ServletException {
         engine = new TemplateEngine();
@@ -27,12 +30,43 @@ public class TimeServlet extends HttpServlet {
         resolver.setOrder(engine.getTemplateResolvers().size());
         resolver.setCacheable(false);
         engine.addTemplateResolver(resolver);
-
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("/text/html");
+        resp.setContentType("text/html; charset=utf-8");
+
+        String timezone = req.getParameter("timezone");
+        if (timezone == null) {
+            Cookie[] cookies = req.getCookies();
+            if (cookies != null) {
+                for (Cookie cookie : cookies) {
+                    if (cookie.getName().equals("lastTimezone")) {
+                        timezone = cookie.getValue();
+                        break;
+                    }
+                }
+            }
+            if (timezone == null) {
+                timezone = "UTC";
+            }
+        } else {
+            timezone = timezone.replace(' ', '+');
+
+            Cookie cookie = new Cookie("lastTimezone", timezone);
+            cookie.setMaxAge(30 * 24 * 60 * 60);
+            resp.addCookie(cookie);
+        }
+
+        String currentTime = ZonedDateTime
+                .now(ZoneId.of(timezone))
+                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss ")) + timezone;
+
+        resp.setContentType("text/html");
+        Context context = new Context();
+        context.setVariable("time", currentTime);
+        engine.process("test", context, resp.getWriter());
+
         resp.getWriter().close();
     }
 }
