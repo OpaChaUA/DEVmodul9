2b7ed5ed069aa0b1f56bd4a2b28b59b0d963baa7
OpaChaUA <opaCha.ua.94@gmail.com>
2023-06-08 19:47:57 +0300

    Initial commit

---
+++ src/main/java/modul9/TimeServlet.java
@@ -0,0 +1,40 @@
+package modul9;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/test")
+public class TimeServlet extends HttpServlet {
+    private TemplateEngine engine;
+    @Override
+    public void init() throws ServletException {
+        engine = new TemplateEngine();
+
+        FileTemplateResolver resolver = new FileTemplateResolver();
+        resolver.setPrefix("/templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setOrder(engine.getTemplateResolvers().size());
+        resolver.setCacheable(false);
+        engine.addTemplateResolver(resolver);
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("/text/html");
+
+        Context simplContext = new Context();
+        engine.process("test.html", simplContext, resp.getWriter());
+        resp.getWriter().close();
+    }
+}
